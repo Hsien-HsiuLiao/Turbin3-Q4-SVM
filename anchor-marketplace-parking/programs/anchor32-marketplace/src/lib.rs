55dfbd25c34428f835b27dc2b20e8f74978f5962
Hsien-Hsiu <hsien_hsiu.liao@yahoo.com>
2025-10-27 23:35:01 -0700

    try open channel ix

--- anchor-marketplace-parking/programs/anchor32-marketplace/src/lib.rs
+++ anchor-marketplace-parking/programs/anchor32-marketplace/src/lib.rs
@@ -1,6 +1,8 @@
 #![allow(unexpected_cfgs, deprecated)]
 
 use anchor_lang::prelude::*;
+use paytube_svm::{transaction::PayTubeTransaction, PayTubeChannel};
+
 
 declare_id!("6kuEXwmR9k4qAWmPcuxR2WisLkQfPJAQS55GQwJ6UmtN");
 
@@ -23,6 +25,31 @@ pub mod anchor32_marketplace {
         Ok(())
     }
 
+    pub fn open_channel(ctx: Context<OpenChannel>) -> Result<()> {
+        let alice = Keypair::new();
+        let bob = Keypair::new();
+        let will = Keypair::new();
+    
+        let alice_pubkey = alice.pubkey();
+        let bob_pubkey = bob.pubkey();
+        let will_pubkey = will.pubkey();
+    
+        let accounts = vec![
+            (alice_pubkey, system_account(10_000_000)),
+            (bob_pubkey, system_account(10_000_000)),
+            (will_pubkey, system_account(10_000_000)),
+        ];
+    
+        let context = TestValidatorContext::start_with_accounts(accounts);
+        let test_validator = &context.test_validator;
+        let payer = context.payer.insecure_clone();
+    
+        let rpc_client = test_validator.get_rpc_client();
+    
+        //opens a channel
+        let paytube_channel = PayTubeChannel::new(vec![payer, alice, bob, will], rpc_client);
+        Ok(())
+    }
     /*
        pub fn reserve(ctx: Context<Reserve>, start_time: i64, end_time: i64) -> Result<()> {
         ctx.accounts.reserve_listing(start_time, end_time)?;
