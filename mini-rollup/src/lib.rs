d26bf4c18dab6315a5fe82205d39bef08e9b62e8
Hsien-Hsiu <hsien_hsiu.liao@yahoo.com>
2025-10-27 23:05:25 -0700

    create state channel

--- mini-rollup/src/lib.rs
+++ mini-rollup/src/lib.rs
@@ -1,14 +1,14 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+use {
+    
+    solana_client::rpc_client::RpcClient,
+   
+    solana_svm::transaction_processor::{
+        TransactionProcessingConfig, TransactionProcessingEnvironment,
+    },
+    
+};
+
+pub struct StateChannel {
+
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
